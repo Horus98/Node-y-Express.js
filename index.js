dc0b422e9da3506cbfc3f1141241c86ab8ebf907
Horus98 <42451015+Horus98@users.noreply.github.com>
2022-03-28 13:51:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 
 const app = express()
-dotenv.config()
+    //dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
