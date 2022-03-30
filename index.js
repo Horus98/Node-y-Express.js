113ef7d2529d43c9cf67ebd3487cdeaf28c0a10b
Maxi Montenegro <maximontenegro512@gmail.com>
2022-03-30 18:56:34 -0300

    update env variable

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.ex5zz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)))
     .catch((error) => console.log(error.message))
 
