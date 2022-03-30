dda0be9e4385cd35a3a1a3f2fd7bcbf9e897ae47
Horus98 <42451015+Horus98@users.noreply.github.com>
2022-03-30 09:57:51 -0300

    Probando Actions

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -4,23 +4,23 @@ import request from "supertest"
 //Ejecucion Test
 //set NODE_OPTIONS=--experimental-vm-modules && npx jest --detectOpenHandles
 
-describe("GET /users", () =>{
+describe("GET /users", () => {
     test("Should respond with status 200", async() => {
         const response = await request(app).get("/users").send()
     })
 })
 
-describe("POST /users", () =>{
+describe("POST /users", () => {
     test("Should respond with status 200", async() => {
         const user = {
             firstName: "Horus",
             lastName: "Moro",
             address: "Av.Siempre Viva",
-            age:99
+            age: 99
         }
         const response = await request(app).post("/users").send(user)
         const idUser = response.body._id
-        const responseUser = await request(app).get("/users/"+idUser).send()
+        const responseUser = await request(app).get("/users/" + idUser).send()
         expect(responseUser.status).toBe(200)
     })
 })
