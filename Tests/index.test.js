4dc9923b1122b919e65e1b248a7d0c0f5ea18b36
Horus <horusmoro2015@gmail.com>
2022-03-29 21:48:41 -0300

    Algunos test basicos

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -7,6 +7,20 @@ import request from "supertest"
 describe("GET /users", () =>{
     test("Should respond with status 200", async() => {
         const response = await request(app).get("/users").send()
-        console.log(response);
+    })
+})
+
+describe("POST /users", () =>{
+    test("Should respond with status 200", async() => {
+        const user = {
+            firstName: "Horus",
+            lastName: "Moro",
+            address: "Av.Siempre Viva",
+            age:99
+        }
+        const response = await request(app).post("/users").send(user)
+        const idUser = response.body._id
+        const responseUser = await request(app).get("/users/"+idUser).send()
+        expect(responseUser.status).toBe(200)
     })
 })
