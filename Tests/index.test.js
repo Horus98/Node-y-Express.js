8c08f514d3dc868e60c4488c1ba396c0ec377e85
Horus <horusmoro2015@gmail.com>
2022-03-31 16:34:49 -0300

    Agrego Tests nuevos

--- Tests/index.test.js
+++ Tests/index.test.js
@@ -23,6 +23,29 @@ describe("POST /users", () => {
     })
 })
 
+describe("DELETE /users", () => {
+    test("Should respond with status 404", async() => {
+        const user = {
+            firstName: "Probando",
+            lastName: "Eliminar",
+            address: "Av.Siempre Viva",
+            age: 200
+        }
+        const response = await request(app).post("/users").send(user)
+        const idUser = response.body._id
+        await request(app).delete("/users/" + idUser).send()
+        const userDeleted = await request(app).get("/users/"+ idUser).send()
+        expect(userDeleted.status).toBe(404)
+    })
+})
+
+describe("POST invalid User", () => {
+    test("Should respond with status 409", async() => {
+        const emptyUser = {}
+        const response = await request(app).post("/users").send(emptyUser)
+        expect(response.status).toBe(409)
+    })
+})
 afterAll(() => {
     return closeConnection();
 });
